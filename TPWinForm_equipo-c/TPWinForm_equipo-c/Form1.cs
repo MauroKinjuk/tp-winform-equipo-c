95713477b416784c3b1f05d3d9303987ead581e9
Mauro <mauro.kinjuk@gmail.com>
2026-09-11 19:24:17 -0300

    Form1

--- TPWinForm_equipo-c/TPWinForm_equipo-c/Form1.cs
+++ TPWinForm_equipo-c/TPWinForm_equipo-c/Form1.cs
@@ -16,5 +16,10 @@ namespace TPWinForm_equipo_c
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }

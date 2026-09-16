868d6a0bf9fbd9c5fa06d329531a5b12935c7bfc
Mauro <mauro.kinjuk@gmail.com>
2026-09-15 23:04:43 -0300

    Se añade listar a Categoria y Marca

--- TPWinForm_equipo-c/negocio/CategoriaNegocio.cs
+++ TPWinForm_equipo-c/negocio/CategoriaNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,31 @@ namespace negocio
 {
     internal class CategoriaNegocio
     {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT Id, Descripcion FROM Categorias");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }

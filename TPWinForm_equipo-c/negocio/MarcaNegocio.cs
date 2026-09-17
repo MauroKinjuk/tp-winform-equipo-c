1f84f736c66e6989ad94c0bce097bef0b82aec23
Lucio Di Francisco <luciodifrancisco@gmail.com>
2026-09-17 16:28:31 -0300

    Se agregan metodos para agregar, modificar y eliminar Marcas

--- TPWinForm_equipo-c/negocio/MarcaNegocio.cs
+++ TPWinForm_equipo-c/negocio/MarcaNegocio.cs
@@ -6,7 +6,7 @@ namespace negocio
 {
     public class MarcaNegocio
     {
-        public List<Marca> listar() 
+        public List<Marca> listar()
         {
             List<Marca> lista = new List<Marca>();
             AccesoDatos datos = new AccesoDatos();
@@ -41,6 +41,48 @@ namespace negocio
             {
                 datos.setearConsulta("insert into MARCAS (Descripcion) values (@Descripcion)");
                 datos.setearParametro("@Descripcion", marca.Descripcion);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(Marca marca)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("update MARCAS set Descripcion = @descripcion where id = @id");
+                datos.setearParametro("@descripcion", marca.Descripcion);
+                datos.setearParametro("@id", marca.Id);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("delete from marcas where id = @id");
+                datos.setearParametro("@id", id);
                 datos.ejecutarAccion();
             }
             catch (Exception ex)

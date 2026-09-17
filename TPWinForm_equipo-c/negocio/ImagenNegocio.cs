0068a0626b263a24037712c8f0b65a7f39df769f
Mauro <mauro.kinjuk@gmail.com>
2026-09-16 21:00:43 -0300

    Se añaden funciones de agregar, listar y eliminar imagenes

--- TPWinForm_equipo-c/negocio/ImagenNegocio.cs
+++ TPWinForm_equipo-c/negocio/ImagenNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,69 @@ namespace negocio
 {
     internal class ImagenNegocio
     {
+        public void agregar(Imagen imagen)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("insert into imagenes (idArticulo, ImagenUrl) values (@idArticulo, @ImagenUrl)");
+                datos.setearParametro("@idArticulo", imagen.IdArticulo);
+                datos.setearParametro("@ImagenUrl", imagen.ImagenUrl);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public List<Imagen> listarPorArticulo(int idArticulo) { 
+            List<Imagen> lista = new List<Imagen>();
+            AccesoDatos datos = new AccesoDatos();
+            try { 
+                datos.setearConsulta("select Id, IdArticulo, ImagenUrl from imagenes where IdArticulo = @idArticulo");
+                datos.setearParametro("@idArticulo", idArticulo);
+                datos.ejecutarLectura();
+                while (datos.Lector.Read()) {
+                    Imagen aux = new Imagen();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminarPorArticulo(int idArticulo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("delete from imagenes where IdArticulo = @idArticulo");
+                datos.setearParametro("@idArticulo", idArticulo);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
